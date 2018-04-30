ca26df9cb5dc6c2ecc71c83a77853ac8d23b9f97
Alec Holmes <alecholmes310@gmail.com>
2018-04-29 18:34:13 -0700

    Refactor giant main function

---
+++ app/differ.go
@@ -0,0 +1,292 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"github.com/alecholmes/tdiff/importer"
+	"github.com/alecholmes/tdiff/lib"
+)
+
+type Logger func(string, ...interface{})
+
+func NoLogging(string, ...interface{}) {}
+
+type Package struct {
+	ImportPath   string   `json:"name"`
+	PathFromRoot []string `json:"pathFromRoot,omitempty"`
+}
+
+type Commit struct {
+	SHA              string     `json:"sha"`
+	Description      string     `json:"description"`
+	RelevantPackages []*Package `json:"relevantPackages,omitempty"`
+}
+
+type Summary struct {
+	RootImportPath string     `json:"rootImportPath,omitempty"`
+	SHA            string     `json:"sha,omitempty"`
+	Packages       []*Package `json:"packages,omitempty"`
+	Commits        []*Commit  `json:"commits,omitempty"`
+	Files          []string   `json:"files,omitempty"`
+}
+
+type Differ struct {
+	goPath         string
+	importer       func(string) (*importer.PackageGraph, error)
+	includeCommits bool
+	includePaths   bool
+	logger         Logger
+}
+
+func NewDiffer(goPath string, importer func(string) (*importer.PackageGraph, error), includeCommits, includePaths bool, logger Logger) *Differ {
+	return &Differ{
+		goPath:         goPath,
+		importer:       importer,
+		includeCommits: includeCommits,
+		includePaths:   includePaths,
+		logger:         logger,
+	}
+}
+
+func (d *Differ) Diff(importPath, sha string, artifacts bool) (*Summary, error) {
+	diff := diff{
+		summary: Summary{
+			RootImportPath: importPath,
+			SHA:            sha,
+		},
+	}
+
+	if err := diff.determineRelevantPackages(d.goPath, artifacts, d.logger); err != nil {
+		return nil, err
+	}
+
+	if err := diff.createPackageSummaries(d.includePaths); err != nil {
+		return nil, err
+	}
+
+	diff.determineRelevantFiles()
+
+	if err := diff.determineCommits(); err != nil {
+		return nil, err
+	}
+
+	return &diff.summary, nil
+}
+
+type diff struct {
+	summary Summary
+
+	git                  *lib.Git
+	graph                *importer.PackageGraph
+	relevantPackages     lib.StringSet       // Relevant packages that changed
+	packageSummaries     map[string]*Package // Summaries by package import path
+	changedArtifactFiles []string            // Artifacts that changed
+	changedPackageFiles  map[string][]string // Files that changed by package
+	changedFilePackage   map[string]string   // Package name by file
+}
+
+func (d *diff) determineRelevantPackages(goPath string, artifacts bool, logger Logger) error {
+	packageNamer, git, err := newGitPackageNamer(d.summary.RootImportPath, goPath, logger)
+	d.git = git
+	if err != nil {
+		return err
+	}
+
+	// Find all files that changed since the given SHA.
+	// Not all files will be relevant, as some will be in unreachable packages.
+	files, err := git.DiffFiles(d.summary.SHA, "HEAD")
+	if err != nil {
+		return err
+	}
+
+	// Determine all the packages with changes.
+	d.changedPackageFiles = make(map[string][]string)
+	d.changedFilePackage = make(map[string]string)
+	// var changedArtifactFiles []string
+	for _, file := range files {
+		packageName := packageNamer(filepath.Dir(file))
+		if strings.HasSuffix(file, ".go") {
+			d.changedPackageFiles[packageName] = append(d.changedPackageFiles[packageName], file)
+			d.changedFilePackage[file] = packageName
+		} else if artifacts && sameOrChildImportPath(packageName, d.summary.RootImportPath) {
+			d.changedArtifactFiles = append(d.changedArtifactFiles, file)
+		}
+	}
+
+	// Find all packages recursively reachable from the given root package.
+	//reachablePackages, err := recursiveDeps(packageFlag)
+	reachablePackages, packageGraph, err := recursiveDeps(d.summary.RootImportPath)
+	if err != nil {
+		return err
+	}
+	d.graph = packageGraph
+
+	// Add given root package to the reachable set
+	reachablePackages = append(reachablePackages, d.summary.RootImportPath)
+
+	// Determine relevant packages, where relevant is a package reachable from
+	// the root package that has also changed.
+	d.relevantPackages = make(lib.StringSet)
+	for _, pkg := range reachablePackages {
+		if _, ok := d.changedPackageFiles[pkg]; ok {
+			d.relevantPackages.Add(pkg)
+		}
+	}
+
+	return nil
+}
+
+func (d *diff) createPackageSummaries(includePaths bool) error {
+	d.packageSummaries = make(map[string]*Package)
+	outPackages := d.relevantPackages.Slice()
+	sort.Strings(outPackages)
+
+	for _, pkg := range outPackages {
+		packageSummary := &Package{ImportPath: pkg}
+		d.summary.Packages = append(d.summary.Packages, packageSummary)
+		d.packageSummaries[pkg] = packageSummary
+
+		if includePaths {
+			shortestPath, err := d.graph.ShortestPath(d.summary.RootImportPath, pkg)
+			if err != nil {
+				return err
+			}
+			if len(shortestPath) == 0 {
+				return fmt.Errorf("Expected path between %s and %s", d.summary.RootImportPath, pkg)
+			}
+			packageSummary.PathFromRoot = shortestPath
+		}
+	}
+
+	return nil
+}
+
+func (d *diff) determineRelevantFiles() {
+	outFiles := d.changedArtifactFiles
+	for pkg := range d.relevantPackages {
+		outFiles = append(outFiles, d.changedPackageFiles[pkg]...)
+	}
+
+	sort.Strings(outFiles)
+	d.summary.Files = outFiles
+}
+
+func (d *diff) determineCommits() error {
+	commits, err := d.git.Commits(d.summary.SHA, "HEAD")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Determine all files that changed in the relevant subset of changed packages.
+	relevantFiles := make(lib.StringSet)
+	for pkg := range d.relevantPackages {
+		relevantFiles.Add(d.changedPackageFiles[pkg]...)
+	}
+	relevantFiles.Add(d.changedArtifactFiles...)
+
+	var relevantCommits []lib.GitCommit
+	for _, commit := range commits {
+		commitFiles, err := d.git.CommitFiles(commit.SHA)
+		if err != nil {
+			return err
+		}
+
+		// The commit should be included if any files in it were part of a relevant changed package.
+		relevant := false
+		for _, file := range commitFiles {
+			if relevantFiles.Contains(file) {
+				relevant = true
+				break
+			}
+		}
+		if relevant {
+			relevantCommits = append(relevantCommits, commit)
+
+			commitPackageSet := make(lib.StringSet)
+			for _, file := range commitFiles {
+				commitPackageSet.Add(d.changedFilePackage[file])
+			}
+
+			commitPackages := commitPackageSet.Slice()
+			sort.Strings(commitPackages)
+			var commitPackageSummaries []*Package
+			for _, commitPackage := range commitPackages {
+				if summary, ok := d.packageSummaries[commitPackage]; ok {
+					commitPackageSummaries = append(commitPackageSummaries, summary)
+				}
+			}
+
+			d.summary.Commits = append(d.summary.Commits, &Commit{
+				SHA:              commit.SHA,
+				Description:      commit.Description,
+				RelevantPackages: commitPackageSummaries,
+			})
+
+		}
+	}
+
+	return nil
+}
+
+// GoPackagerNamer determines a full package name given a partial package name.
+type goPackagerNamer func(partialPackageName string) string
+
+// NewGitPackageNamer creates a goPackagerNamer and Git based on the
+// given package name and Go path.
+// This function will attempt to use the Go path to determine the root of
+// the Git repository the given package lives under.
+func newGitPackageNamer(importPath, goPath string, logger Logger) (goPackagerNamer, *lib.Git, error) {
+	srcDir := fmt.Sprintf("%s/src/", goPath)
+	logger("Using source directory: %s", srcDir)
+
+	git, err := lib.NewGitInDir(fmt.Sprintf("%s%s", srcDir, importPath))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	logger("Using git root: %s", git.RootDir)
+
+	if !strings.HasPrefix(git.RootDir, srcDir) {
+		return nil, nil, fmt.Errorf("Expected git root to be under %s; working directory is %s", srcDir, git.RootDir)
+	}
+
+	packagePrefix := git.RootDir[len(srcDir):]
+	logger("Prefixing packages with: %s", packagePrefix)
+
+	return func(relativePackage string) string {
+		return fmt.Sprintf("%s/%s", packagePrefix, relativePackage)
+	}, git, nil
+}
+
+// Returns true if the given import path is either the same package or a child
+// of selfOrParentImportPath.
+// sameOrChildImportPath("a/b", "a/b") == true
+// sameOrChildImportPath("a/b/c", "a/b") == true
+// sameOrChildImportPath("a", "a/b") == false
+func sameOrChildImportPath(importPath, sameOrParentImportPath string) bool {
+	if !strings.HasPrefix(importPath, sameOrParentImportPath) {
+		return false
+	} else if len(importPath) == len(sameOrParentImportPath) {
+		return true
+	}
+
+	return importPath[len(sameOrParentImportPath)] == '/'
+}
+
+func recursiveDeps(packageName string) ([]string, *importer.PackageGraph, error) {
+	graph, err := importer.DefaultRecursiveImport(packageName)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	packageNames := make([]string, 0, len(graph.Packages))
+	for name := range graph.Packages {
+		packageNames = append(packageNames, name)
+	}
+
+	return packageNames, graph, nil
+}
--- main.go
+++ main.go
@@ -6,37 +6,19 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
-	"sort"
-	"strings"
 
+	"github.com/alecholmes/tdiff/app"
 	"github.com/alecholmes/tdiff/importer"
-	"github.com/alecholmes/tdiff/lib"
 )
 
-type Package struct {
-	Name         string   `json:"name"`
-	PathFromRoot []string `json:"pathFromRoot,omitempty"`
-}
-
-type Commit struct {
-	SHA              string     `json:"sha"`
-	Description      string     `json:"description"`
-	RelevantPackages []*Package `json:"relevantPackages,omitempty"`
-}
-
-type Summary struct {
-	Packages []*Package `json:"packages,omitempty"`
-	Commits  []*Commit  `json:"commits,omitempty"`
-	Files    []string   `json:"files,omitempty"`
-}
-
 var (
-	packageFlag   = flag.String("package", "", "Package to find reachable diff from")
-	shaFlag       = flag.String("sha", "", "Git SHA")
-	artifactsFlag = flag.Bool("artifacts", false, "If true, includes changed non-Go source files under the package directory, recursive")
+	// Required input flags
+	packageFlag = flag.String("package", "", "Package to find reachable diff from")
+	shaFlag     = flag.String("sha", "", "Git SHA")
 
-	verboseFlag = flag.Bool("verbose", false, "If set, log verbose debugging information")
+	// Optional flags
+	artifactsFlag = flag.Bool("artifacts", false, "If true, includes changed non-Go source files under the package directory, recursive")
+	verboseFlag   = flag.Bool("verbose", false, "If set, log verbose debugging information")
 
 	// Output format flags
 	packagesFlag = flag.Bool("packages", false, "If set, all relevant changed packages printed")
@@ -52,160 +34,34 @@ func main() {
 		os.Exit(1)
 	}
 
-	// TODO: Check that exactly one of the output flags is set.
-	packageNamer, git, err := newGitPackageNamer(*packageFlag, *verboseFlag)
+	includePaths := *jsonFlag
+	logger := app.NoLogging
+	if *verboseFlag {
+		logger = log.Printf
+	}
+
+	differ := app.NewDiffer(os.Getenv("GOPATH"), importer.DefaultRecursiveImport, *commitsFlag, includePaths, logger)
+
+	summary, err := differ.Diff(*packageFlag, *shaFlag, *artifactsFlag)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Find all files that changed since the given SHA.
-	// Not all files will be relevant, as some will be in unreachable packages.
-	files, err := git.DiffFiles(*shaFlag, "HEAD")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Determine all the packages with changes.
-	changedPackageFiles := make(map[string][]string)
-	changedFilePackage := make(map[string]string)
-	var changedArtifactFiles []string
-	for _, file := range files {
-		packageName := packageNamer(filepath.Dir(file))
-		if strings.HasSuffix(file, ".go") {
-			changedPackageFiles[packageName] = append(changedPackageFiles[packageName], file)
-			changedFilePackage[file] = packageName
-		} else if *artifactsFlag && selfOrChildPackage(packageName, *packageFlag) {
-			changedArtifactFiles = append(changedArtifactFiles, file)
+	if *packagesFlag {
+		for _, pkg := range summary.Packages {
+			fmt.Println(pkg.ImportPath)
 		}
 	}
 
-	// Find all packages recursively reachable from the given root package.
-	//reachablePackages, err := recursiveDeps(packageFlag)
-	reachablePackages, packageGraph, err := newRecursiveDeps(*packageFlag)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Add given root package to the reachable set
-	reachablePackages = append(reachablePackages, *packageFlag)
-
-	// Determine relevant packages, where relevant is a package reachable from
-	// the root package that has also changed.
-	relevantPackages := make(stringSet)
-	for _, pkg := range reachablePackages {
-		if _, ok := changedPackageFiles[pkg]; ok {
-			relevantPackages.add(pkg)
+	if *filesFlag {
+		for _, file := range summary.Files {
+			fmt.Println(file)
 		}
 	}
 
-	summary := new(Summary)
-	packageSummaries := make(map[string]*Package)
-
-	if *packagesFlag || *jsonFlag {
-		outPackages := relevantPackages.slice()
-		sort.Strings(outPackages)
-
-		for _, pkg := range outPackages {
-			shortestPath, err := packageGraph.ShortestPath(*packageFlag, pkg)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if len(shortestPath) == 0 {
-				log.Fatalf("Expected path between %s and %s", *packageFlag, pkg)
-			}
-
-			if *jsonFlag {
-				packageSummary := &Package{
-					Name:         pkg,
-					PathFromRoot: shortestPath,
-				}
-				summary.Packages = append(summary.Packages, packageSummary)
-				packageSummaries[pkg] = packageSummary
-			}
-
-			if *packagesFlag {
-				fmt.Println(pkg)
-			}
-		}
-	}
-
-	if *filesFlag || *jsonFlag {
-		outFiles := changedArtifactFiles
-		for pkg := range relevantPackages {
-			outFiles = append(outFiles, changedPackageFiles[pkg]...)
-		}
-
-		sort.Strings(outFiles)
-		for _, file := range outFiles {
-			if *jsonFlag {
-				summary.Files = append(summary.Files, file)
-			}
-
-			if *filesFlag {
-				fmt.Println(file)
-			}
-		}
-	}
-
-	if *commitsFlag || *jsonFlag {
-		commits, err := git.Commits(*shaFlag, "HEAD")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Determine all files that changed in the relevant subset of changed packages.
-		relevantFiles := make(stringSet)
-		for pkg := range relevantPackages {
-			relevantFiles.add(changedPackageFiles[pkg]...)
-		}
-		relevantFiles.add(changedArtifactFiles...)
-
-		var relevantCommits []lib.GitCommit
-		for _, commit := range commits {
-			commitFiles, err := git.CommitFiles(commit.SHA)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// The commit should be included if any files in it were part of a relevant changed package.
-			relevant := false
-			for _, file := range commitFiles {
-				if relevantFiles.contains(file) {
-					relevant = true
-					break
-				}
-			}
-			if relevant {
-				relevantCommits = append(relevantCommits, commit)
-
-				if *jsonFlag {
-					commitPackageSet := make(stringSet)
-					for _, file := range commitFiles {
-						commitPackageSet.add(changedFilePackage[file])
-					}
-
-					commitPackages := commitPackageSet.slice()
-					sort.Strings(commitPackages)
-					var commitPackageSummaries []*Package
-					for _, commitPackage := range commitPackages {
-						if summary, ok := packageSummaries[commitPackage]; ok {
-							commitPackageSummaries = append(commitPackageSummaries, summary)
-						}
-					}
-
-					summary.Commits = append(summary.Commits, &Commit{
-						SHA:              commit.SHA,
-						Description:      commit.Description,
-						RelevantPackages: commitPackageSummaries,
-					})
-				}
-			}
-		}
-
-		if *commitsFlag {
-			for _, commit := range relevantCommits {
-				fmt.Printf("%s %s\n", commit.SHA, commit.Description)
-			}
+	if *commitsFlag {
+		for _, commit := range summary.Commits {
+			fmt.Printf("%s %s\n", commit.SHA, commit.Description)
 		}
 	}
 
@@ -216,130 +72,4 @@ func main() {
 		}
 		fmt.Println(string(body))
 	}
-}
-
-// stringSet represents an unordered set of strings.
-type stringSet map[string]bool
-
-// add puts the given values into the set.
-func (s stringSet) add(values ...string) {
-	for _, v := range values {
-		s[v] = true
-	}
-}
-
-// contains returns true iff the set contains the given value.
-// s.contains("foo") and s["foo"] are equivalent.
-func (s stringSet) contains(value string) bool {
-	return s[value]
-}
-
-// slice returns an arbitrarily ordered slice that contains each value in the set.
-func (s stringSet) slice() []string {
-	values := make([]string, 0, len(s))
-	for v := range s {
-		values = append(values, v)
-	}
-
-	return values
-}
-
-// goPackagerNamer determines a full package name given a partial package name.
-type goPackagerNamer func(partialPackageName string) string
-
-// newGitPackageNamer creates a goPackagerNamer and Git based on the current Go environment
-// and a given package name.
-// This function will attempt to use the GOPATH environment variable to figure out the root of
-// the Git repository the given package lives under.
-func newGitPackageNamer(packageName string, verbose bool) (goPackagerNamer, *lib.Git, error) {
-	srcDir := fmt.Sprintf("%s/src/", os.Getenv("GOPATH"))
-	if verbose {
-		log.Printf("Using source directory: %s", srcDir)
-	}
-
-	git, err := lib.NewGitInDir(fmt.Sprintf("%s%s", srcDir, packageName))
-	if err != nil {
-		return nil, nil, err
-	}
-
-	if verbose {
-		log.Printf("Using git root: %s", git.RootDir)
-	}
-
-	if !strings.HasPrefix(git.RootDir, srcDir) {
-		return nil, nil, fmt.Errorf("Expected git root to be under %s; working directory is %s", srcDir, git.RootDir)
-	}
-
-	packagePrefix := git.RootDir[len(srcDir):]
-	if verbose {
-		log.Printf("Prefixing packages with: %s", packagePrefix)
-	}
-
-	return func(relativePackage string) string {
-		return fmt.Sprintf("%s/%s", packagePrefix, relativePackage)
-	}, git, nil
-}
-
-// Returns true if the given packageName is either the same package or a child
-// of selfOrParentPackageName.
-// selfOrParentPackageName("a/b", "a/b") == true
-// selfOrParentPackageName("a/b/c", "a/b") == true
-// selfOrParentPackageName("a", "a/b") == false
-func selfOrChildPackage(packageName, selfOrParentPackageName string) bool {
-	if !strings.HasPrefix(packageName, selfOrParentPackageName) {
-		return false
-	} else if len(packageName) == len(selfOrParentPackageName) {
-		return true
-	}
-
-	return packageName[len(selfOrParentPackageName)] == '/'
-}
-
-func newRecursiveDeps(packageName string) ([]string, *importer.PackageGraph, error) {
-	graph, err := importer.DefaultRecursiveImport(packageName)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	packageNames := make([]string, 0, len(graph.Packages))
-	for name := range graph.Packages {
-		packageNames = append(packageNames, name)
-	}
-
-	return packageNames, graph, nil
-}
-
-func recursiveDeps(root *string) ([]string, error) {
-	args := []string{"list", "-f", "'{{.Deps}}'"}
-	if root != nil {
-		args = append(args, *root)
-	}
-	out, err := lib.RunCommand("go", args...)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(out) == 0 {
-		return nil, nil
-	}
-
-	str := string(out)
-	// This assumes that `go list` outputs as Go-formatted array of strings.
-	// E.g. '[]', '[foo]', '[foo bar]'
-	str = strings.Trim(str, "'[]\n")
-
-	return strings.Split(str, " "), nil
-}
-
-// goDirs take a list of filenames, filters out ones that are not Go source files,
-// and returns the set of parent directories of these files.
-func goDirs(files []string) stringSet {
-	dirSet := make(stringSet)
-	for _, file := range files {
-		if strings.HasSuffix(file, ".go") {
-			dirSet.add(filepath.Dir(file))
-		}
-	}
-
-	return dirSet
 }
